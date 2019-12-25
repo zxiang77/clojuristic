4e5b54383f6cc8162b5e900956f1cba24e082f0e
zxiang77 <zxiang77@yahoo.com>
2019-12-25 15:20:28 -0500

    added toy code for reference, wip login system

--- src/models/login.clj
+++ src/models/login.clj
@@ -1,8 +1,31 @@
 (ns models.login)
 
-(defrecord loginform [uname, upass, token])
 
-(defn makeLogin [uname, upass, token]
-  (->loginform uname upass token))
+; define a protocol for authentication
+; mock data retrieval from db
+(defprotocol Auth
+  (auth []))
+
+(defrecord loginform [uname, upass]
+    Auth
+    (auth []
+      (let [data (lookup "user" uname, upass)] ;; more destructing may needed here
+       )))
+
+(defrecord tokenform [token]
+    Auth
+  (auth []
+    ()))
+
+(defn lookup [table ..name]
+  ())
+
+(defn makeLogin [uname, upass]
+  (->loginform uname upass))
+
+(defn makeToken [token]
+  (->tokenform [token]))
+
+
 
 
--- src/toys/toy.clj
+++ src/toys/toy.clj
@@ -12,4 +12,61 @@
 (defn maketoy [a b c]
   (toyimpl. a b c))
 
+;; set tooling
+(def seteg #{"Alice", "Bob", "Kelly"})
+(conj seteg "new elm") ; adding element
+(disj seteg "Alice") ; removing element
+(contains? seteg "Alice") ; checking containment
+(conj (sorted-set) "bravo" "hack" "abbrev")
+(into seteg #{"fa"}) ; join
+
+;; map tooling
+(def scores {"Fred"  1400
+   "Bob"   1240
+   "Angela" 1024})
+
+;; same as put() in java
+(assoc scores "Sally" 0)
+;; rm data
+(dissoc scores "Bob")
+;; get
+(get scores "Angela")
+(scores :Angela) ; if the map is used as constant lookup table
+(:Angela scores) ; if the map is used as constant lookup table
+
+;; get with default
+(get scores "Angela" 22)
+(scores :Angela 22) ; if the map is used as constant lookup table
+
+;; all keys, all vals
+(keys scores)
+(vals scores)
+
+;; if macro is used when we have if else structure
+(if (even? 5)
+  (do (println "even")
+    true)
+  (do (println "odd")
+    false))
+
+;; when is used when only 1 then no else
+(when (neg? x)
+  (throw (RuntimeException. (str "x must be positive: " x))))
+
+;; it's for a series of if conditions, :else is optional
+(let [x 11]
+  (cond
+    (< x 2)  "x is less than 2"
+    (< x 10) "x is less than 10"
+    :else  "x is greater than or equal to 10"))
+
+;; switch loop
+(defn foo [x]
+  (case x
+    5 "x is 5"
+    10 "x is 10"
+    "x isn't 5 or 10"))
+
+
+
 ;; place multimethods here
